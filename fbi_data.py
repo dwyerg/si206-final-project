d7cbee7d602fcab111396c7a3efb78b2d9615d26
Grace Dwyer <dwyerg@umich.edu>
2021-12-01 19:48:41 -0500

    creating key tables

--- fbi_data.py
+++ fbi_data.py
@@ -22,41 +22,52 @@ def setUpDatabase(db_name):
     return cur, conn
 
 def create_criminal_table(cur, conn):
-    cur.execute("CREATE TABLE IF NOT EXISTS Criminals (criminal_id INTEGER, name TEXT, dob_used TEXT, race TEXT, field_office TEXT, sex TEXT, crimes TEXT, reward INTEGER)")
+    cur.execute("CREATE TABLE IF NOT EXISTS Criminals (name TEXT PRIMARY KEY, dob_used TEXT, race TEXT, field_office TEXT, sex TEXT, crimes TEXT, reward INTEGER)")
     conn.commit()
     # make criminal_id into INTEGER PRIMARY KEY
 
-def add_criminal(cur, conn):
-    response = requests.get('https://api.fbi.gov/wanted/v1/list')
-    data = json.loads(response.content)
-    criminal_id = 0
+def add_criminals(cur, conn):
+    page = 1
+    num_criminals = 0
+    while num_criminals < 100:
+        params={'page': page}
+        response = requests.get('https://api.fbi.gov/wanted/v1/list', params=params)
+        data = json.loads(response.content)
+        page += 1
 
-    for item in data['items']:
-        criminal_id += 1
-        name = item['title']
-        if item['dates_of_birth_used'] == None:
-            dob_used = "no dob"
-        else:
-            dob_used = item['dates_of_birth_used'][0]
-        race = item['race']
-        if item['field_offices'] == None:
-            field_office = "no location"
-        else:
-            field_office = item['field_offices'][0]
-        sex = item['sex']
-        crimes = item['description']
-        reward = item['reward_text']
-   
-        cur.execute("INSERT INTO Criminals (criminal_id, name, dob_used, race, field_office, sex, crimes, reward) VALUES (?, ?, ?, ?, ?, ?, ?, ?)", (criminal_id, name, dob_used, race, field_office, sex, crimes, reward))
+        for item in data['items']:
+            num_criminals += 1
+            name = item['title']
+            if item['dates_of_birth_used'] == None:
+                dob_used = "no dob"
+            else:
+                dob_used = item['dates_of_birth_used'][0]
+            race = item['race']
+            if item['field_offices'] == None:
+                continue
+            else:
+                field_office = item['field_offices'][0]
+            sex = item['sex']
+            crimes = item['description']
+            reward = item['reward_text']
+    
+            cur.execute("INSERT OR IGNORE INTO Criminals (name, dob_used, race, field_office, sex, crimes, reward) VALUES (?, ?, ?, ?, ?, ?, ?)", (name, dob_used, race, field_office, sex, crimes, reward))
 
     conn.commit()
 
-
+def get_field_offices(cur, conn):
+    cur.execute("SELECT field_office FROM Criminals")
+    offices = cur.fetchall()
+    field_offices = {}
+    for office in offices:
+        if office not in field_offices:
+            field_offices[office[0]] = ""
+    print(field_offices)
 
 cur, conn = setUpDatabase("FBI_data.db")
-create_criminal_table(cur, conn)
-add_criminal(cur, conn)
-
+# create_criminal_table(cur, conn)
+# add_criminals(cur, conn)
+get_field_offices(cur, conn)
 
 
 
